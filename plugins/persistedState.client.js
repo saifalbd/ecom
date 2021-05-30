1384b8b9536b824418a487f8c5a67b49f20e0baa
saifalbd <saifalwebs@gmail.com>
2021-05-31 00:16:11 +0600

    configapiToken

--- plugins/persistedState.client.js
+++ plugins/persistedState.client.js
@@ -2,6 +2,6 @@ import createPersistedState from 'vuex-persistedstate'
 export default ({ store }) => {
   createPersistedState({
     key: 'vuex-persisted',
-    paths: ['ShopingCart', 'auth']
+    paths: ['ShopingCart', 'auth', 'api_token']
   })(store)
 }
