83222a25b847620326ca47467dbe2414e438f878
saifalbd <saifalwebs@gmail.com>
2021-06-04 03:43:27 +0600

    axfix

--- plugins/axios/index.js
+++ plugins/axios/index.js
@@ -1,15 +1,17 @@
 export default ({ $axios, redirect }, inject) => {
+  $axios.defaults.headers.common.token =
+    'b97500c3c086f33d38923c9adf96e6b2bce7579e57a45d25e4641ad5992fbf46'
   // store.dispatch('addApiToken', process.env.API_TOKEN)
-  try {
-    if (typeof window !== 'undefined') {
-      $axios.defaults.headers.common.token = window.app.app_access_key
-      console.log('this should print only if window object actually exists')
-    } else {
-      $axios.defaults.headers.common.token = process.env.API_TOKEN
-    }
-  } catch (error) {
-    $axios.defaults.headers.common.token = process.env.API_TOKEN
-  }
+  // try {
+  //   if (typeof window !== 'undefined') {
+  //     $axios.defaults.headers.common.token = window.app.app_access_key
+  //     console.log('this should print only if window object actually exists')
+  //   } else {
+  //     $axios.defaults.headers.common.token = 'b97500c3c086f33d38923c9adf96e6b2bce7579e57a45d25e4641ad5992fbf46'
+  //   }
+  // } catch (error) {
+  //   $axios.defaults.headers.common.token = process.env.API_TOKEN
+  // }
 
   inject('axiosWithoutToken', $axios)
 
